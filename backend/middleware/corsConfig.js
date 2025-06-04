e9577f1ea874aa1db4482d87f641d054ee573894
Dilkush <dilkushsingh974@gmail.com>
2025-06-04 14:15:26 +0530

    cors otpimized

--- backend/middleware/corsConfig.js
+++ backend/middleware/corsConfig.js
@@ -1,20 +1,5 @@
-const allowedOrigins = [
-  'http://127.0.0.1:5500',
-  'http://localhost:5500',
-  'http://127.0.0.1:5501',
-  'http://localhost:5501',
-  'https://manage-task-frontend-url.onrender.com',
-  'https://task-flow-24mp.onrender.com'
-];
-
 const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: '*', // Allow all origins
   credentials: true
 };
 
